716304128de7223f415d4eff211bcb4d991f6a0c
Owais-Iqbal4 <owais123iqbal@gmail.com>
2025-09-06 17:32:14 +0500

    all extra code removed from these files

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -3,10 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
-  AiFillGithub,
   AiOutlineWhatsApp
 } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
   return (
